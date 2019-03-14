067e136348500ae548199dee1198234960aed945
Bruno Henrique <brunobr9@hotmail.com>
2019-03-14 00:06:05 -0300

    Commit Inicial

---
+++ src/main/java/com/brunobr9/cursomc/BrunoSpringBootAppApplication.java
@@ -0,0 +1,12 @@
+package com.brunobr9.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunoSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunoSpringBootAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/brunobr9/cursomc/BrunoSpringBootAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brunobr9.cursomc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrunoSpringBootAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
